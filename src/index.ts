6875d6b35fd547b7ef7abed10e9d181ae3911a52
“GlennChia” <glennchia7@gmail.com>
2021-03-10 20:46:24 +0800

    feat: add 1 level object key finder with array

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 // eslint-disable-next-line
 type interfaceObject = {[key: string]: any};
 
+// eslint-disable-next-line
+function isArray(value: any): boolean{
+  return Object.prototype.toString.call(value) == '[object Array]';
+}
+
 // Assume that the keys of the object are all strings
 function objectLineFinderRecursion(targetObject: interfaceObject, target: string, _value?: string): Array<number[] | number> {
   const matchingLines = [];
   // If object { first key starts at line 2
   let line = 2;
-  // eslint-disable-next-line
-  for (const [key, _value] of Object.entries(targetObject)) {
+  for (const [key, value] of Object.entries(targetObject)) {
     if (key == target) {
       matchingLines.push(line);
+    }
+    if (isArray(value)){
+      if (typeof(value[0]) != 'object') {
+        // Add 1 for the closing bracket
+        line += value.length + 1;
+      }
     }
     line += 1;
   }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,6 @@
 import objectLineFinder from "../src/index";
 import * as dataTest1 from "./examples/test1.json";
+import * as dataTest2 from "./examples/test2.json";
 
 test("Testset 1: finds the line number of firstName", () => {
   expect(objectLineFinder(dataTest1, 'firstName')).toStrictEqual([2]);
@@ -7,4 +8,16 @@ test("Testset 1: finds the line number of firstName", () => {
 
 test("Testset 1: finds the line number of age", () => {
   expect(objectLineFinder(dataTest1, 'age')).toStrictEqual([4]);
+});
+
+test("Testset 2: finds the line number of firstName", () => {
+  expect(objectLineFinder(dataTest2, 'firstName')).toStrictEqual([2]);
+});
+
+test("Testset 2: finds the line number of hobbies", () => {
+  expect(objectLineFinder(dataTest2, 'hobbies')).toStrictEqual([4]);
+});
+
+test("Testset 2: finds the line number of age", () => {
+  expect(objectLineFinder(dataTest2, 'age')).toStrictEqual([8]);
 });
